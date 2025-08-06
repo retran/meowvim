6ad55cfa43529d09049d680c67d462372c5c76d8
Andrew Vasilyev <me@retran.me>
2025-08-06 18:29:48 +0200

    fix transparency in headless mode

--- lua/plugins/tokyonight.lua
+++ lua/plugins/tokyonight.lua
@@ -1,12 +1,22 @@
--- lua/plugins/tokyonight.lua
-
 return {
   "folke/tokyonight.nvim",
   lazy = false,
   priority = 1000,
   config = function()
+    local function is_headless()
+      if vim.v.argv == nil then
+        return false
+      end
+      for _, arg in ipairs(vim.v.argv) do
+        if arg == "--headless" then
+          return true
+        end
+      end
+      return false
+    end
+
     require("tokyonight").setup({
-      transparent = not vim.g.neovide,
+      transparent = not vim.g.neovide and not is_headless(),
       styles = {
         sidebars = "transparent",
         floats = "transparent",
